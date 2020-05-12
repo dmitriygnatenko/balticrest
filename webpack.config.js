f95c1601fda0843f531379e2aebc5e8e09d98813
Dmitriy <info@dmitriygnatenko.ru>
2020-05-12 22:13:34 +0200

    Update assets

--- webpack.config.js
+++ webpack.config.js
@@ -8,32 +8,36 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
 
-    .addEntry('balticrest_js_jquery', './assets/balticrest/js/jquery.min.js')
+    // Balticrest
+
+    .addEntry('balticrest_js_jquery', './assets/balticrest/jquery/js/jquery.min.js')
 
     .addEntry('balticrest_js_ie9', [
-        './assets/balticrest/js/html5shiv.min.js',
-        './assets/balticrest/js/respond.min.js',
+        './assets/balticrest/html5shiv/js/html5shiv.min.js',
+        './assets/balticrest/respond/js/respond.min.js',
     ])
 
     .addEntry('balticrest_js_main', [
-        './assets/balticrest/js/bootstrap.min.js',
-        './assets/balticrest/js/all_scr.js',
-        './assets/balticrest/js/wow.min.js',
-        './assets/balticrest/js/jquery.parallax-0.2-min.js',
+        './assets/balticrest/bootstrap/js/bootstrap.min.js',
+        './assets/balticrest/balticrest/js/all_scr.js',
+        './assets/balticrest/wow/js/wow.min.js',
+        './assets/balticrest/jquery/js/jquery.parallax-0.2-min.js',
     ])
 
     .addEntry('balticrest_css_main', [
-        './assets/balticrest/css/bootstrap.css',
-        './assets/balticrest/css/main.css',
-        './assets/balticrest/css/adaptive.css',
-        './assets/balticrest/css/pushy.css',
-        './assets/balticrest/css/animate.css',
-        './assets/balticrest/css/font-awesome.css',
+        './assets/balticrest/bootstrap/css/bootstrap.css',
+        './assets/balticrest/balticrest/css/main.css',
+        './assets/balticrest/balticrest/css/adaptive.css',
+        './assets/balticrest/pushy/css/pushy.css',
+        './assets/balticrest/animate/css/animate.css',
+        './assets/balticrest/font-awesome/css/font-awesome.css',
     ])
 
     .addEntry('balticrest_js_map_index', [
-        './assets/balticrest/js/map_index.js',
+        './assets/balticrest/balticrest/js/map_index.js',
     ])
+
+    // Admin
 
     // https://symfony.com/doc/current/frontend.html#adding-more-features
     .cleanupOutputBeforeBuild()
