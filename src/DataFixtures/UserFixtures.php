f436bfdf4135d78ee53652ffc63d9be33c1574be
Dmitriy Gnatenko <d.gnatenko@skyeng.ru>
2020-09-14 21:43:04 +0200

    Refactoring

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -34,7 +34,7 @@ class UserFixtures extends Fixture
             ->setRoles([UserRolesInterface::ROLE_USER])
             ->setEmail('test@balticrest.ru')
             ->setIsActive(true)
-             ->setPassword($this->passwordEncoder->encodePassword($user, '12345'));
+            ->setPassword($this->passwordEncoder->encodePassword($user, '12345'));
 
         $manager->persist($user);
         $manager->flush();
