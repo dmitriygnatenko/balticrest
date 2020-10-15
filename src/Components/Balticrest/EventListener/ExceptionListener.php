e31f4ffc00abec7688ddf5a98e177febc1a24faf
Dmitriy Gnatenko <d.gnatenko@skyeng.ru>
2020-10-15 23:04:03 +0200

    404

--- src/Components/Balticrest/EventListener/ExceptionListener.php
+++ src/Components/Balticrest/EventListener/ExceptionListener.php
@@ -4,15 +4,36 @@ declare(strict_types=1);
 
 namespace App\Components\Balticrest\EventListener;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
 
 class ExceptionListener
 {
+    /** @var Environment */
+    private $twig;
+
+    /**
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @param ExceptionEvent $event
      */
     public function onKernelException(ExceptionEvent $event)
     {
-        // TODO
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $response = new Response();
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            $response->setContent($this->twig->render('balticrest/error/404.html.twig'));
+            $event->setResponse($response);
+        }
     }
 }
