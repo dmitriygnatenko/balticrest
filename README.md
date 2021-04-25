2c7eecdf01f29b9978ba589e7d35e182c983eca2
Dmitriy Gnatenko <d.gnatenko@skyeng.ru>
2021-04-25 22:37:45 +0200

    Add view mainpage tests

--- README.md
+++ README.md
@@ -7,5 +7,10 @@
 2. bin/console doctrine:fixtures:load  --env=test
 3. ./vendor/bin/codecept run
 
+### Commands
+1. bin/console balticrest:update-schedule
+2. bin/console balticrest:update-search
+3. bin/console balticrest:update-sitemap
+
 ### Deploy (on prod env)
 ./deploy.sh
--- tests/functional/MainPageTestCest.php
+++ tests/functional/MainPageTestCest.php
@@ -1,51 +1,76 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests;
 
 use App\Tests\FunctionalTester;
 
-class PagesCest
+class MainPageTestCest
 {
-    public function mainPage(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewRu(FunctionalTester $I)
     {
         $I->amOnPage('/');
         $I->wantTo('View main page (RU)');
-        $I->seeResponseCodeIs(200);
+        $I->seePageIsAvailable();
         $I->see('ТУРИСТИЧЕСКИЙ СПРАВОЧНИК БАЛТИЙСКОГО ПОБЕРЕЖЬЯ');
     }
 
-    public function mainPageEn(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewEn(FunctionalTester $I)
     {
         $I->amOnPage('/en');
         $I->wantTo('View main page (EN)');
+        $I->seePageIsAvailable();
         $I->see('TOURIST GUIDE TO THE BALTIC COAST');
     }
 
-    public function mainPageDe(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewDe(FunctionalTester $I)
     {
         $I->amOnPage('/de');
         $I->wantTo('View main page (DE)');
+        $I->seePageIsAvailable();
         $I->see('REISEFÜHRER AN DER OSTSEEKÜSTE');
     }
 
-    public function mainPagePl(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewPl(FunctionalTester $I)
     {
         $I->amOnPage('/pl');
         $I->wantTo('View main page (PL)');
+        $I->seePageIsAvailable();
         $I->see('PRZEWODNIK TURYSTYCZNY PO WYBRZEŻU BAŁTYKU');
     }
 
-    public function mainPageLt(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewLt(FunctionalTester $I)
     {
         $I->amOnPage('/lt');
         $I->wantTo('View main page (LT)');
+        $I->seePageIsAvailable();
         $I->see('BALTIJOS PAKRANTĖS TURISTINIS VADOVAS');
     }
 
-    public function mainPageCn(FunctionalTester $I)
+    /**
+     * @param \App\Tests\FunctionalTester $I
+     */
+    public function viewCn(FunctionalTester $I)
     {
         $I->amOnPage('/cn');
         $I->wantTo('View main page (CN)');
+        $I->seePageIsAvailable();
         $I->see('波羅的海沿岸旅遊指南');
     }
 }
